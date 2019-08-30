e13c173528e80da49d6b8ed2eaec35b16577209c
2ndyrGroupC <2ndyrGroupC@pn.local>
2019-08-30 20:12:09 +0800

    MQTT

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,41 +1,110 @@
 // basic functionalities
-client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
-client.subscribe("mqtt/demo")
+$(document).ready(function () {
 
-client.on("connect", function(){
-    console.log("Successfully connected");
-})
+  client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt");
+  var data = '';
+  var x = '';
+  var con = false;
+  var a = false;
 
-client.on("message", function (topic, payload) {
-  console.log([topic, payload].join(": "));
-  client.end();
-})
+  document.getElementById('btnSubscribe').addEventListener("click", function (e) {
+    e.preventDefault();
+    a = false;
+    if (con == true) {
+      if ($('#Subscribe-topic').val() == '') {
+        alert('No Topic provided!')
+      } else {
+        client.subscribe($('#Subscribe-topic').val());
+        data = 'You have subscribed to the topic ' + $('#Subscribe-topic').val();
+        $('#span').html(data);
+        x = $('#Subscribe-topic').val();
+      }
+    } else {
+      alert('Connect first!');
+    }
+  })
 
-client.publish("mqtt/demo", "hello world!")
+  document.getElementById('btnUnsubscribe').addEventListener("click", function (e) {
+    e.preventDefault();
+    a = true;
+    $('#table').html('');
+    $('#span').html('');
+    $('#Subscribe-topic').val('');
+  })
 
-// // advance functionalities
-// client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
-// client.subscribe("mqtt/demo", function (err){
-//   if (err){
-//     console.log(err);
-//   } else {
-//     console.log("subscribed")
-//   }
-// })
 
-// client.on("connect", function(){
-//     console.log("Successfully connected");
-// })
+  client.publish("mqtt/demo", "hello world!");
 
-// client.on("message", function (topic, payload) {
-//   console.log([topic, payload].join(": "));
-//   client.end();
-// })
+  document.getElementById('btnConnect').addEventListener("click", function (e) {
+    e.preventDefault();
+    con = true;
+    client.on("connect", function () {
+      console.log("Successfully connected");
+    })
+    $('#btnSubscribe').attr('disabled',false);
+  })
 
-// client.publish("mqtt/demo", "hello world!", function(err){
-//   if (err){
-//     console.log(err)
-//   } else {
-//     console.log("published")
-//   }
-// })
+  document.getElementById('btnDisconnect').addEventListener("click", function (e) {
+    e.preventDefault();
+    data = '';
+    console.log('You are disconnected.');
+    client.end();
+    $('#span').html(data);
+    $('#Subscribe-topic').val('');
+    $('#Publish-topic').val('');
+    $('#Publish-payload').val('');
+    $('#btnSubscribe').attr('disabled',true);
+  })
+
+  document.getElementById('btnPublish').addEventListener("click", function (e) {
+    e.preventDefault();
+    console.log('Published.');
+    if (a == true) {
+      alert('Subscribe first!')
+    } else
+      if (con == true) {
+        if ($('#Publish-topic').val() == '' && $('#Publish-payload').val() == '') {
+          alert('No Topic provided!')
+        } else {
+          client.on("message", function (topic, payload) {
+            console.log($('#Publish-topic').val(), $('#Publish-payload').val());
+          })
+          client.publish($('#Publish-topic').val(), $('#Publish-payload').val());
+          if (x == $('#Publish-topic').val()) {
+            var dt = new Date();
+            var time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+            $('table').append('<tr><td>'+$('#Publish-topic').val()+'</td><td>'+$('#Publish-payload').val()+'</td><td>'+time+'</td></tr>');
+          }
+        }
+      } else {
+        alert('Subscribe first!')
+      }
+  })
+
+  // // advance functionalities
+  // client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
+  // client.subscribe("mqtt/demo", function (err){
+  //   if (err){
+  //     console.log(err);
+  //   } else {
+  //     console.log("subscribed")
+  //   }
+  // })
+
+  // client.on("connect", function(){
+  //     console.log("Successfully connected");
+  // })
+
+  // client.on("message", function (topic, payload) {
+  //   console.log([topic, payload].join(": "));
+  //   client.end();
+  // })
+
+  // client.publish("mqtt/demo", "hello world!", function(err){
+  //   if (err){
+  //     console.log(err)
+  //   } else {
+  //     console.log("published")
+  //   }
+  // })
+});
